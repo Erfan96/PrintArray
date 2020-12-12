90f57988b0e5280e4683ac0168ada73fd6da4bf6
efsam <E.salimi75@ymail.com>
2020-12-12 20:10:09 +0330

    commit Arrays for Main in Secondary

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
 public class Main {
     public static void main(String[] args) {
+        Sample sample = new Sample();
+        Character[] characters = {'a', 'd', 'f', 'k', 'h', 'n'};
+        Integer[] integers = {10, 1, 5, 4, 8, 15};
+        Double[] doubles = {10.1, 25.7, 5.3, 6.64, 7.5};
 
+        sample.printArray(characters);
+        sample.printArray(integers);
+        sample.printArray(doubles);
     }
 }
