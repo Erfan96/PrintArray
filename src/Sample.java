7e1bb5bbece0d6541e0818bb8bac5086d6de5b58
efsam <E.salimi75@ymail.com>
2020-12-12 20:44:43 +0330

    commit overload method for Sample in Secondary

--- src/Sample.java
+++ src/Sample.java
@@ -1,8 +1,21 @@
 
 public class Sample {
-    public <T> void printArray(T[] array){
-           for (T arr : array){
-               System.out.println(arr);
-           }
+    public <T> void printArray(T[] array) {
+        for (T arr : array) {
+            System.out.println(arr);
+        }
+
     }
+
+    public <T> void printArray(T[] array, int lowSubscript, int highSubscript){
+        if ((lowSubscript < 1 || lowSubscript > array.length) || highSubscript > array.length)
+            throw new IllegalArgumentException("Has illegalArgumentException .");
+
+        else {
+            for (int i=lowSubscript ; i<=highSubscript ; i++){
+                System.out.println(array[i - 1]);
+            }
+        }
+    }
+
 }
