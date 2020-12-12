676e11075ecbd4bf80df395e077e1e05c5d8a313
efsam <E.salimi75@ymail.com>
2020-12-12 20:44:08 +0330

    commit execute overload method for Main in Secondary

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,16 @@
 public class Main {
     public static void main(String[] args) {
         Sample sample = new Sample();
-        Character[] characters = {'a', 'd', 'f', 'k', 'h', 'n'};
+        Character[] characters = {'a', 'd', 'f', 'k', 'h', 'R'};
         Integer[] integers = {10, 1, 5, 4, 8, 15};
         Double[] doubles = {10.1, 25.7, 5.3, 6.64, 7.5};
 
         sample.printArray(characters);
         sample.printArray(integers);
         sample.printArray(doubles);
+
+        sample.printArray(characters, 1, 6);
+        sample.printArray(integers, 1, 6);
+        sample.printArray(doubles,1, 5);
     }
 }
